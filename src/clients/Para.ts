d501c2a488a4279b24335fd94ca00e86f45be8e8
MANUEL RIOS <many.rios56@gmail.com>
2025-04-11 17:01:06 -0400

    new changes two

--- src/clients/Para.ts
+++ src/clients/Para.ts
@@ -1,12 +1,6 @@
 import ParaWeb, { Environment } from "@getpara/react-sdk";
-import { createParaAccount, createParaViemClient } from "@getpara/viem-v2-integration";
-import { http } from "viem";
-import {  sepolia } from "viem/chains";
 
 const CapsuleParasApi = import.meta.env.VITE_PARA_API_KEY;
-
-//const RPC_BASESEPOLIA = import.meta.env.VITE_RPC_BASESEPOLIA as string;
-const RPC_ETHSEPOLIA = import.meta.env.VITE_RPC_ETHSEPOLIA as string;
 
 if(!CapsuleParasApi){
     throw new Error("Please set Para API for use capsule");
@@ -17,18 +11,4 @@ const para = new ParaWeb(Environment.BETA, CapsuleParasApi);
 if (!para) {
   throw new Error("Failed to initialize paraViemClient");
 }
-
-export const paraAccount = createParaAccount(para);
-
-if(!paraAccount){
-  throw new Error("Failed to initialize paraAccount");
-}
-
-
-export const paraViemClient = createParaViemClient(para, {
-  account: paraAccount,
-  chain: sepolia,
-  transport: http(RPC_ETHSEPOLIA),
-});
-
 export default para;
